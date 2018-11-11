869d5ca61042dbec8c74dc369c24372f4a27dc40
rosolomo <44929813+rosolomo@users.noreply.github.com>
2018-11-11 07:53:26 -0500

    Add files via upload

---
+++ gglex.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import datetime
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+Id = '3qaaig988gtuu337vek48ncg10@group.calendar.google.com'
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+    
+    
+# Refer to the Python quickstart on how to setup the environment:
+# https://developers.google.com/calendar/quickstart/python
+# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+# stored credentials.
+
+    
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId=Id, timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    elist=list(range(len(events)))
+    for i in range(len(events)):
+        
+        start = events[i]['start'].get('dateTime', events[i]['start'].get('date'))            
+        elist[i]= [start,events[i]['summary'],events[i]['description']]
+       # print(start, events[i]['summary'],events[i]['description'])
+    return elist
+
+if __name__ == '__main__':
+    main()
